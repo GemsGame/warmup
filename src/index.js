6ad7e7e99d8fa8da6d372740e3981f98f885e016
GemsGame <support@gems.cash>
2018-09-12 01:23:53 +0300

    C to F, done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+   if (temperature !== undefined)
+        {
+         return temperature*1.8+32;
+        }
+    //Пример: 10°C x 1,8 + 32 = 50°F
+    
+    /* undefined
+var answer = warmup("0");
+console.log(answer); */   
 };
+
+
